28edf2b2daeb9ae68d4362341ae6bc3b8570b917
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-25 06:35:11 +0500

    Innitial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Masala: a[0], a[1], a[2], ....a[n] massiv yarating va uni qiymatlarini hisoblang qoshish orqali
+
+const a=[];
+
+let n=5;
+let sum=0;
+for(i=0; i<=n; i++){  
+    a.push(i);
+    sum = sum +a[i];
+
+}
+
+console.log(sum);
+
+
+
+
